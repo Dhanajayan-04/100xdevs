e981927552ed299ffed4373041334d22c44fca5d
Dhanajayan <dhanajayanyasotha@gmail.com>
2026-07-22 21:15:08 +0530

    completed the week-7 01-profile-card assignment

---
+++ week-7/01-profile-card/src/App.jsx
@@ -0,0 +1,20 @@
+
+import ProfileComponent from "./components/ProfileComponent";
+
+function App() {
+
+  return (
+    <>
+      <ProfileComponent 
+      name="Dhanajayan"
+      age={22}
+      city="Chennai"
+      followers={6000}
+      likes= {3000}
+      photos= {40}
+      />
+    </>
+  )
+}
+
+export default App
---
+++ week-7/01-profile-card/src/components/ProfileComponent.jsx
@@ -0,0 +1,29 @@
+
+function ProfileComponent(props) {
+    return <div className="card">
+        <div className="user-info">
+            <h2>{props.name} <span>{props.age}</span></h2>
+            <p>{props.city}</p>
+        </div>
+
+        <div className="stats">
+        <div className="stat">
+            <h2>{props.followers}</h2>
+            <p>Followers</p>
+        </div>
+
+        <div className="stat">
+            <h2>{props.likes}</h2>
+            <p>Likes</p>
+        </div>
+
+        <div className="stat">
+            <h2>{props.photos}</h2>
+            <p>Photos</p>
+        </div>
+    
+        </div>
+    </div>
+}
+
+export default ProfileComponent;
