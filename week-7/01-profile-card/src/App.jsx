458dcfcf49dbd2d78d61cae5fd28fecdab73b307
Dhanajayan <dhanajayanyasotha@gmail.com>
2026-07-23 19:22:51 +0530

    Completed the week-7 assignment 01-profile-card sucessfully

--- week-7/01-profile-card/src/App.jsx
+++ week-7/01-profile-card/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <ProfileComponent 
+      profileUrl= 'https://imgs.search.brave.com/1KiyzuDRcHPqmU55ezONAKkAiaxJjIqKAentGsEkpco/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2FlLzMx/L2M4L2FlMzFjODEz/M2JhNzUzYTBmZDYx/OGE1MGJmNzhmNTZk/LmpwZw'
       name="Dhanajayan"
       age={22}
       city="Chennai"
--- week-7/01-profile-card/src/components/ProfileComponent.jsx
+++ week-7/01-profile-card/src/components/ProfileComponent.jsx
@@ -1,6 +1,12 @@
 
+import "./ProfileComponent.css";
+
 function ProfileComponent(props) {
     return <div className="card">
+        <div className="coverPic">
+        <img src={props.profileUrl} alt="" />
+        </div>
+
         <div className="user-info">
             <h2>{props.name} <span>{props.age}</span></h2>
             <p>{props.city}</p>
