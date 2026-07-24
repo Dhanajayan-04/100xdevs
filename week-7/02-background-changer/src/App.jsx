bfde5199e75ec210197177291194da1ac3487769
Dhanajayan <dhanajayanyasotha@gmail.com>
2026-07-24 19:15:31 +0530

    Completed the week-7 assignment 02-background-changer sucessfully

--- week-7/02-background-changer/src/App.jsx
+++ week-7/02-background-changer/src/App.jsx
@@ -1,26 +1,30 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState("white");
 
   useEffect(() => {
-    document.body.style.background = color;
-  }, [color])
-
-  const changeBG = (e) => {
-    setColor(e.target.innerText.toLowerCase())
-  }
+    document.body.style.backgroundColor = color;
+  }, [color]);
 
   return (
     <div className="toolbar">
-        <span onClick={changeBG}>Red</span>
-        <span onClick={changeBG}>Yellow</span>
-        <span onClick={changeBG}>Purple</span>
-        <span onClick={changeBG}>Green</span>
-        <span onClick={changeBG}>Blue</span>
-        <span onClick={changeBG}>Orange</span>
+      <button className="red" onClick={() => setColor("red")}>Red</button>
+
+      <button className="yellow" onClick={() => setColor("yellow")}>Yellow</button>
+
+      <button className="black" onClick={() => setColor("black")}>Black</button>
+
+      <button className="purple" onClick={() => setColor("purple")}>Purple</button>
+
+      <button className="green" onClick={() => setColor("green")}>Green</button>
+
+      <button className="blue" onClick={() => setColor("blue")}>Blue</button>
+
+      <button className="default" onClick={() => setColor("white")}>Default</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
