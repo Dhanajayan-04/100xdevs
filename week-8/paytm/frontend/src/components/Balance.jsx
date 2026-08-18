04eb811de3d1db4b5fd4d1df657ab1ff3c578d99
Dhanajayan <dhanajayanyasotha@gmail.com>
2026-08-18 14:46:28 +0530

    update balance component and other files

--- week-8/paytm/frontend/src/components/Balance.jsx
+++ week-8/paytm/frontend/src/components/Balance.jsx
@@ -1,5 +1,5 @@
 
-export const balance = ({ value }) => {
+export const Balance = ({ value }) => {
     return <div className="flex">
         <div className="font-bold text-lg">
             Your balance
